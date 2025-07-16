13fcffb065501436db4fe5517613e8f7878ecee1
TatsuyaTakigawa <taki0701tty@gmail.com>
2025-07-16 12:25:34 +0900

    SQL文を修正

--- source/php/home.php
+++ source/php/home.php
@@ -1,18 +1,20 @@
 <?php
     require_once 'common_login.php';
 ?>
+
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <title>Aso商店</title>
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/menu2.css">
-    <link rel="stylesheet" href="../css/main.css">
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Aso商店</title>
+        <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="../css/menu2.css">
+        <link rel="stylesheet" href="../css/main.css">
+    </head>
+    <body>
+
 <?php
-require_once 'menu.php';
+    require_once 'menu.php';
 ?>
 <?php
     //DB接続
@@ -26,24 +28,49 @@ require_once 'menu.php';
 
 ?>
 
-<div id="column" class="column04">
-    <h3>商品一覧</h3>
-    <ul>
-        <?php
-        foreach ($stmt as $row){
-            echo '<li>';
-            //商品を表示
-            echo '<a href="./itemdetail.php?id=',$row['id'],'">';
-            echo '<img class="item" src="../itemimg/',$row['imgpath'],'" />';
-            echo '<p>',$row['name'],'</p>';
-            echo '<span>',$row['price'],'円</span>';
-            echo '</a>';
-            echo '';
-            echo '</li>';
-        }
-        ?>
-    </ul>
-</div>
+        <div id="column" class="column04">
+            <h3>商品一覧</h3>
+            <ul>
+                <?php
+                foreach ($stmt as $row){
+                    echo '<li>';
+                    //商品を表示
+                    echo '<a href="./itemdetail.php?id=',$row['id'],'">';
+                    echo '<img class="item" src="../itemimg/',$row['imgpath'],'" />';
+                    echo '<p>',$row['name'],'</p>';
+                    echo '<span>',$row['price'],'円</span>';
+                    echo '</a>';
+                    echo '';
+                    echo '</li>';
+                }
+                ?>
+            </ul>
+        </div>
 
-</body>
+<?php
+    require_once 'recommend_functions.php';
+    $recommendedProducts = getRecommendedProducts((string)$_SESSION['user']['id'], 4);
+    $user_name = $_SESSION['user']['name'];
+?>
+
+        <div id="column" class="column04">
+            <h3><?php echo htmlspecialchars($user_name, ENT_QUOTES, 'UTF-8'); ?>さんへのオススメ商品</h3>
+            <ul>
+                <?php
+                foreach ($recommendedProducts as $row){
+                    echo '<li>';
+                    //商品を表示
+                    echo '<a href="./itemdetail.php?id=',$row['id'],'">';
+                    echo '<img class="item" src="../itemimg/',$row['imgpath'],'" />';
+                    echo '<p>',$row['name'],'</p>';
+                    echo '<span>',$row['price'],'円</span>';
+                    echo '</a>';
+                    echo '';
+                    echo '</li>';
+                }
+                ?>
+            </ul>
+        </div>
+
+    </body>
 </html>
